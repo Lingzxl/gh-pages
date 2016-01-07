d0e5c0bb8264c99bc0159315753c684b808c3641
Lingzxl <1039558719@qq.com>
2016-01-07 18:50:05 +0800

    额鹅鹅鹅

---
+++ js/login.js
@@ -0,0 +1,57 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2016-01-07 15:52:05
+ * @version $Id$
+ */
+  // 记住我切换图片效果
+ function change_pic(){
+	var imgObj = document.getElementById("caocao_pic");
+	var Flag=(imgObj.getAttribute("src",2)=="images/u234.png")
+	imgObj.src=Flag?"images/u123.png":"images/u234.png";
+ }
+
+ $(document).ready(function() {
+  
+   $(".error_msg").hide();
+   
+   $('.btn_login').click(function(e) {     
+	    //点击登陆  		     
+		var name = $.trim($('input[name=name]').val());  
+		var passwd = $.trim($('input[name=password]').val()); 
+		// alert(name); 
+		// alert(passwd); 
+		if (name == '') {   
+			$(".error_msg").html('请填写用户名');  
+			$(".error_msg").show().fadeOut(3500);
+			$('input[name=name]').focus();   
+				return false;
+		}else if (passwd == '') {  
+			$(".error_msg").html('请填写密码');  
+			$(".error_msg").show().fadeOut(3500);  
+			$('input[name=password]').focus(); 
+			    return false; 
+		}else{	
+             $.ajax({			    
+			    type: 'POST',// 请求发送方式			    
+			    url: 'http://120.24.14.108:3000/api/v1/login',// 验证文件			    
+			    data: {'mue': name, 'pass': passwd},// 用户输入的帐号密码			    
+			    async: true,// 异步，不写默认为True
+			    //请求成功后的回调
+			    success: function(data){
+			    	  // alert(JSON.stringify(data));
+			          window.location.href="index.html";
+			    },
+			    //请求失败后的回调
+			    error: function(data){
+				      var tmp='<span>'+data.responseJSON.errmsg+'</span>';
+				      $('.error_msg').empty();
+			          $('.error_msg').append(tmp);
+			          $(".error_msg").show().fadeOut(3500);   
+			          window.location.href="#";      
+			    }
+		      }); 
+            }
+		 e.preventDefault();
+    }); 
+ });
